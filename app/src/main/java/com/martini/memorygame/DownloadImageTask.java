e3c7dbdf1c9c7ff3e5f43b1bacffef581d0026d2
Martini024 <zhao435021640@gmail.com>
2020-07-15 19:24:17 +0800

    first commit

---
+++ app/src/main/java/com/martini/memorygame/DownloadImageTask.java
@@ -0,0 +1,37 @@
+package com.martini.memorygame;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    private static final String TAG = "DownloadImageTask";
+
+    ImageButton imageButton;
+    public DownloadImageTask(ImageButton imageButton) {
+        System.out.println("Download Image Task started");
+        this.imageButton = imageButton;
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+        String urlDisplay = urls[0];
+        Bitmap bmp = null;
+        try {
+            InputStream in = new java.net.URL(urlDisplay).openStream();
+            bmp = BitmapFactory.decodeStream(in);
+            System.out.println("I get the image");
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+        return bmp;
+    }
+    protected void onPostExecute(Bitmap result) {
+        imageButton.setImageBitmap(result);
+    }
+}
---
+++ app/src/main/java/com/martini/memorygame/LoadActivity.java
@@ -0,0 +1,27 @@
+package com.martini.memorygame;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+
+        // Onclick refresh do ...
+        // Get the urls
+
+        // Get all buttons (20)
+        ImageButton imageButton = findViewById(R.id.imageButton);
+
+        // Download Image and set to ...
+        new DownloadImageTask(imageButton).execute("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSPwIg6mZVGSnb47ItE3wJ_RMga34oO7EuyJA&usqp=CAU");
+        // Drawable drawable = getDrawable(R.drawable.camel);
+
+//        imageButton.setBackground(drawable);
+    }
+}
